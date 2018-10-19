38162ac139255f2f60bcddbf3651228babd27c1c
kojo <annand556@gmail.com>
2018-10-19 15:47:55 +0000

    update

--- mysite/urls.py
+++ mysite/urls.py
@@ -18,6 +18,6 @@ from django.urls import url, include
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls)
-    url(r'', include('blog.urls'))
+    url(r'^admin/', admin.site.urls),
+    url(r'', include('blog.urls')),
 ]
